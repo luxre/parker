9c17e9da4d62bb172fb48ecb7c5224df25e4a560
Bryan Stedman <bryanbstedman@gmail.com>
2020-03-05 17:23:23 -0700

    Initial commit

---
+++ parker.js
@@ -0,0 +1,150 @@
+var parker = (function() {
+
+  // Set initial vars
+  var wh = window.innerHeight;
+  var defaultOptions = {
+    breakpointStart: 768,
+    bottomMargin: 0,
+    parent: false
+  };
+  var parkerObject = {}
+  var parkerIsSticky = false;
+  var options = {};
+  var element = false;
+  var elementProperties, parent, parentProperties, floor;
+  var lastScrollTop = 0;
+  var scrollingDown = true;
+
+  // Init function
+  parkerObject.init = function(selector, opts) {
+
+    // Set up options object
+    options = Object.assign({}, defaultOptions, opts);
+
+    // Get element to make sticky
+    element = document.querySelector(selector);
+
+    if(element) {
+      elementProperties = element.getBoundingClientRect();
+      element.style.width = Math.round(elementProperties.width) + 'px';
+
+      // Look for specified parent element if present
+      parent = options.parentSelector ? document.querySelector(options.parentSelector) : false;
+
+      checkBrowserWidth();
+
+      window.addEventListener('resize', function() {
+        checkBrowserWidth();
+      });
+    }
+
+  };
+
+  // Make sure we are above the min-wdth breakpoint
+  function checkBrowserWidth() {
+    if (window.matchMedia('(min-width: ' + options.breakpointStart + 'px)').matches) {
+      if (!parkerIsSticky) {
+        watchScroll();
+      }
+    } else {
+      window.removeEventListener('scroll', makeSticky);
+      unstick(element, true);
+    }
+  };
+
+  // Watch scrolling event
+  function watchScroll() {
+    window.addEventListener('scroll', makeSticky);
+  };
+
+  // Make the selected element sticky
+  function makeSticky(e) {
+
+    parentProperties = parent ? parent.getBoundingClientRect() : element.parentElement.getBoundingClientRect();
+    floor = parentProperties.bottom;
+    elementProperties = element.getBoundingClientRect();
+    var parentTop = parentProperties.top;
+    var parentOffset = parentProperties.height - wh + parentProperties.y;
+
+    // Check scroll direction
+    var scrollPosition = window.scrollY;
+    if ( scrollPosition > lastScrollTop ){
+      scrollingDown = true;
+    } else {
+      scrollingDown = false;
+    }
+    lastScrollTop = scrollPosition;
+
+    // Make it sticky
+    if (scrollingDown) {
+
+      // Scrolling down
+      if (parkerIsSticky && scrollPosition <= 0) {
+        unstick(element);
+      } else if ((elementProperties.height - window.innerHeight) <= -elementProperties.top) {
+
+        if (!parkerIsSticky) {
+          parkerIsSticky = true;
+
+          element.style.width = Math.round(elementProperties.width) + 'px';
+          element.style.bottom = options.bottomMargin + 'px';
+          element.style.position = 'fixed';
+          element.style.height = elementProperties.height + 'px';
+        }
+
+        if (parentOffset <= 0) {
+          if (element.parentNode.style.position === 'relative') {
+            element.style.position = 'absolute';
+            element.style.bottom = options.bottomMargin + 'px';
+          } else {
+            if (floor) {
+              var diff = wh - floor;
+              element.style.bottom = diff + options.bottomMargin + 'px';
+            }
+          }
+        }
+      } else if (parentProperties.bottom >= wh) {
+      }
+    } else {
+
+      // Scrolling Up
+      if(floor > wh && parkerIsSticky && element.style.position == 'absolute') {
+        element.style.position = 'fixed';
+      } else if(elementProperties.y <= parentTop) {
+        if (parkerIsSticky) {
+          unstick();
+        }
+      } else if(parentTop <= 0) {
+        if (parentProperties.bottom >= wh) {
+        } else {
+          element.style.bottom = options.bottomMargin + 'px';
+          element.style.position = 'fixed';
+          element.style.height = elementProperties.height + 'px';
+          if (floor) {
+            element.style.bottom = wh - floor + options.bottomMargin + 'px';
+          }
+        }
+
+      }
+    }
+  };
+
+  //
+  function unstick(full = false) {
+    element.style.position = '';
+    element.style.top = '';
+    element.style.right = '';
+    element.style.bottom = '';
+    element.style.left = '';
+    parkerIsSticky = false;
+    if (full) {
+      element.style.width = '';
+    }
+    parkerIsSticky = false;
+  };
+
+  return parkerObject;
+
+})();
+
+export default parker
---
+++ readme.md
@@ -0,0 +1,54 @@
+# Parker.js — Make things sticky
+
+Hey web slingers! Parker.js is a small script for making elements sticky.
+
+![](images/parker_sticky.gif)
+
+Parker.js lets you target a given element and will make it stick in place once the bottom of that element scrolls into view. It will unstick it, allowing it to continue scrolling normally, once the bottom of its parent element scrolls to the position of the bottom of the sticky element. The same is true of the tops of the elements when scrolling up.
+
+
+### Installing
+Install parker.js with [Yarn](http://yarnpkg.com)
+
+```
+yarn add parker.js
+```
+
+and import the parker module
+
+```
+import parker from "parker.js"
+```
+
+## Usage
+Kick off parker.js with it's `init` method:  
+```
+parker.init(selector, options)
+```
+
+Parker.js takes a selector of the element that is to be make sticky. If parker cannot find an element that matches the selector it will fail silently. If there are multiple elements that match the given selector it will just use the first one.
+
+In addition to the selector, parker.js accepts an options object which can include the following options:
+
+### Options:
+![](images/parker_options.gif)
+
+**parentSelector**:
+The sticky element will stay in place until the top or bottom of the parent element touches the top or bottom of the sticky element. By default parker.js can infer the parent of the element that is being made sticky. If, however, you would prefer to treat a grandparent or an element higher up the stack as the parent you can set a selector here to have the sticky element remain in place until that element touches the top or bottom of the sticky element.
+
+Using sibling or a different element within which the sticky element is not contained, can cause some… _interesting_ behavior.
+
+**breakpointStart**
+It doesn't typically make sense to make elements sticky at narrow breakpoints. By default, elements will retain standard behavior until the window is wider than 768px. If you'd like to change this you can pass the value of the desired width in pixels.
+
+**bottomMargin**
+By default parker.js does not add any margin between the sticky element and the parent. A pixel value can be set here to provide some buffer between the bottom of the sticky element and the bottom of the parent element.
+
+### Examples:
+```
+parker.init('.parker', {
+  parentSelector: '.parker-parent',
+  breakpointStart: 800,
+  bottomMargin: 50
+});
+```
